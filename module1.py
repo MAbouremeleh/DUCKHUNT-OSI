96b4f69620d819990e2c4dc2d61993e8267704ff
Kevin Kuang <kkuang@osisoft.com>
2018-07-12 15:00:35 -0700

    Revert "Revert "working joystick""
    This reverts commit 49cac891ecd115285172a90aa5bdcfc5ad8a19bc.

--- module1.py
+++ module1.py
@@ -22,39 +22,24 @@ print (_joystick.get_numhats())
 print()
 
 pygame.event.get()
-up_init = int(_joystick.get_axis(1)) #Initalizing up joystick postion
-left_init = int(_joystick.get_axis(0)) #Intializing left joystick postion
-right_init = (_joystick.get_axis(2)) # Initializing right joystick position
-down_init = (_joystick.get_axis(3)) #Initialzing down joystick position
+vert_init = int(_joystick.get_axis(1)) #Initalizing up joystick postion
+horz_init = int(_joystick.get_axis(0)) #Intializing left joystick postion
 
-print(up_init)
-print(left_init)
-print(down_init)
-print(right_init)
-'''
+
+print(vert_init)
+print(horz_init)
 while True:
-    current_up = int(_joystick.get_axis(1))
-    current_left = int(_joystick.get_axis(0))
-    current_right = (_joystick.get_axis(2))
-    current_down = (_joystick.get_axis(3))
-    if  current_up != up_init:
-        print("up") #Vert =-1 means Up
-        print (current_up)
-    elif  current_left != left_init:
-        print("left") #Horz =-1 means Left
-        print (current_left)
-    elif current_right != right_init:
-        print("right")
-        print (current_right)
-    elif current_down != right_init:
-        print("down")
-        print (current_down)
+    current_vert = int(_joystick.get_axis(1)*1.5)
+    current_horz = int(_joystick.get_axis(0)*1.5)
+    if  current_vert != vert_init:
+        print("vert") #Vert =-1 means Up
+        print (current_vert)
+    elif  current_horz != horz_init:
+        print("horz") #Horz =-1 means Left
+        print (current_horz)
     else:
         print("else")
-        #print(current_up)
-        #print(current_left)
-        print(current_right)
-        #print(current_down)
+        print(current_vert)
+        print(current_horz)
         
     (pygame.event.get())
-        '''
